38b53aec1cf06b5dd76335e67088d19e090e92a9
santiago0411 <49250130+santiago0411@users.noreply.github.com>
2024-03-25 00:03:09 -0300

    Read request and send basic response

--- app/server.c
+++ app/server.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
@@ -7,55 +6,119 @@
 #include <errno.h>
 #include <unistd.h>
 
-int main() {
-	// Disable output buffering
-	setbuf(stdout, NULL);
+#ifdef LOCAL_TEST
+#define PORT 30505
+#else
+#define PORT 4221
+#endif
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+#define IN_BUF_SIZE 1024
+static uint8_t IN_BUF[IN_BUF_SIZE];
 
-	// Uncomment this block to pass the first stage
-	int client_addr_len;
+int accept_client(const int socket_fd)
+{
 	struct sockaddr_in client_addr;
+	const int client_addr_len = sizeof(client_addr);
 
-	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	const int client_fd = accept(socket_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	if (client_fd < 0) {
+		fprintf(stderr, "Error accepting connection: %s.\n", strerror(errno));
+		return -1;
+	}
+
+	const uint8_t octets[] = {
+		(client_addr.sin_addr.s_addr >> 0) & 0xFF,
+		(client_addr.sin_addr.s_addr >> 8) & 0xFF,
+		(client_addr.sin_addr.s_addr >> 16) & 0xFF,
+		(client_addr.sin_addr.s_addr >> 24) & 0xFF,
+	};
+
+	printf("Client %d.%d.%d.%d:%d connected!\n",
+		octets[0], octets[1],
+		octets[2], octets[3],
+		client_addr.sin_port
+	);
+
+	return client_fd;
+}
+
+int create_socket(const uint16_t port)
+{
+	const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
+		fprintf(stderr, "Socket creation failed: %s...\n", strerror(errno));
+		return -1;
 	}
 
 	// Since the tester restarts your program quite often, setting REUSE_PORT
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
-		return 1;
+		fprintf(stderr, "SO_REUSEPORT failed: %s \n", strerror(errno));
+		return -1;
 	}
 
 	struct sockaddr_in serv_addr = {
 		.sin_family = AF_INET ,
-		.sin_port = htons(4221),
+		.sin_port = htons(port),
 		.sin_addr = { htonl(INADDR_ANY) },
 	};
 
 	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
+		fprintf(stderr, "Bind failed: %s \n", strerror(errno));
+		return -1;
+	}
+
+	printf("Bound on port %d\n", port);
+	return server_fd;
+}
+
+int main() {
+	// Disable output buffering
+	setbuf(stdout, NULL);
+	printf("Starting socket...\n");
+
+	const int socket_fd = create_socket(PORT);
+	if (socket_fd < 0) {
 		return 1;
 	}
 
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
+	const int connection_backlog = 5;
+	if (listen(socket_fd, connection_backlog) != 0) {
+		fprintf(stderr, "Listen failed: %s \n", strerror(errno));
 		return 1;
 	}
 
 	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
 
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	printf("Client connected\n");
+	for (;;) {
+		const int client_fd = accept_client(socket_fd);
+		if (client_fd < 0) {
+			continue;
+		}
 
-	close(server_fd);
+		int nbytes = recv(client_fd, IN_BUF, IN_BUF_SIZE, 0);
 
+		if (nbytes < 0) {
+			fprintf(stderr, "Failed to read bytes: %s", strerror(errno));
+			close(client_fd);
+			break;
+		}
+
+		printf("%.*s", nbytes, IN_BUF);
+
+		const char* response = "HTTP/1.1 200 OK\r\n\r\n";
+		printf("Sending response:\n%s", response);
+		if (send(client_fd, response, strlen(response), 0) < 0) {
+			fprintf(stderr, "Failed to send response: %s\n", strerror(errno));
+		}
+
+		printf("Response sent successfully\n");
+		// For the time being we only handle one connection
+		close(client_fd);
+		break;
+	}
+
+	close(socket_fd);
 	return 0;
 }
